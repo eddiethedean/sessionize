11d4b39e6032462ee10133a10bf9a30fcef3604c
Odos Matthews <odosmatthews@odoss-mbp.lan>
2021-12-31 09:28:05 -0500

    add python>=3.9 requirement

--- sessionize/__init__.py
+++ sessionize/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.4'
+__version__ = '0.0.6'
 
 # Sessionized
 from sessionize.session_table import SessionTable
--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 import pathlib
 from setuptools import setup, find_packages
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="sessionize",
-    version="0.0.4",
+    version="0.0.6",
     description="Make changes to sql tables using SqlAlchmey sessions",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/eddiethedean/sessionize",
     author="Odos Matthews",
     author_email="odosmatthews@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(),
     include_package_data=True,
+    python_requires='>=3.9',
     install_requires=['sqlalchemy', 'alembic']
 )
