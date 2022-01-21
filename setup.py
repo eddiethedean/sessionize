6cca1687ecda45a45bc989f3a4fc1295a8cb3a00
Odos Matthews <odosmatthews@odoss-mbp.lan>
2022-01-21 11:28:25 -0500

    update README

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sessionize",
-    version="0.0.7",
+    version="0.0.8",
     description="Make changes to sql tables using SqlAlchmey sessions",
     long_description=README,
     long_description_content_type="text/markdown",
